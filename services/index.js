c6a6b53038d1f4f5bd0a7421a11017916cfde42f
guisposito <guicarlosjoinhas@gmail.com>
2023-05-22 22:45:01 -0300

    Criando o Feth APi do hypergraphy

---
+++ services/index.js
@@ -0,0 +1,41 @@
+import { graphql } from "graphql";
+import { request, gql } from "graphql-request";
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export const getPosts = async () => {
+  const query = gql`
+    query MyQuery {
+      query
+      Assets {
+        postsConnection {
+          edges {
+            node {
+              authors {
+                bio
+                name
+                id
+                photo {
+                  url
+                }
+              }
+              createdAt
+              slug
+              title
+              excerpt
+              featuredImage {
+                url
+              }
+              categories {
+                name
+                slug
+              }
+            }
+          }
+        }
+      }
+    }
+  `
+  const results = await request(graphqlAPI, query)
+};
+
